97e39eec07f3a73f6f1ab624a97df0c16c587038
ecodeshub251 <amirkhan251@outlook.com>
2021-05-24 20:35:49 +0530

    toggle feature

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Categories = ({setCurrentCategory, allCategories, currentCategory}) => {
-    
+const Categories = ({setCurrentCategory, products, allCategories, currentCategory, setItems}) => {
+
+
+    useEffect(() => {
+        filterItems(currentCategory)
+    },[])
+    const filterItems = (category) => {
+        const newItems = products.filter(p => p.category === category);
+        setItems(newItems)
+        setCurrentCategory(category)
+    }
     return (
         <nav className='container w-1/2 mx-auto pt-5'>
             <ul className='flex justify-around'>
                 {allCategories.map(category => (
-                    <li key={category} onClick={() => setCurrentCategory(category)} className='text-xl text-primary font-bold cursor-pointer'>
+                    <li key={category} onClick={() => filterItems(category)} className='text-xl text-primary font-bold cursor-pointer'>
                         {category}
                         <div className={`${category === currentCategory && 'underline bg-secondary'  }`} ></div>
                     </li>
--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -8,16 +8,19 @@ const Menu = () => {
 
     const [ currentCategory, setCurrentCategory ] = useState("Fast Food");
     const [ allCategories, setAllCategories ] = useState([])
+    const [ items, setItems ] = useState(products);
+
 
     useEffect(() => {
         const categories = [...new Set( products.map(product => product.category))]
         setAllCategories(categories);
     },[])
 
+    
     return <section>
-        <Categories setCurrentCategory={setCurrentCategory} currentCategory={currentCategory} allCategories={allCategories} />
+        <Categories setCurrentCategory={setCurrentCategory} products={products} setItems={setItems} currentCategory={currentCategory} allCategories={allCategories} />
         <main className='grid grid-cols-2 gap-5 pt-5'>
-        {products.map((product) => {
+        {items.map((product) => {
             return <Product product={product} />
         })}
         </main>
