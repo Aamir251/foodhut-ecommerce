f964400d30885eb2202569df17ba9e8eb7653d7f
ecodeshub251 <amirkhan251@outlook.com>
2021-06-07 17:04:48 +0530

    login screen design

--- client/src/components/Screens/LoginScreen.js
+++ client/src/components/Screens/LoginScreen.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { Link, useHistory, useLocation } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { login } from '../../actions/userActions'
-import ACTIONS from '../../actionTypes/actionTypes'
 
 
 const LoginScreen = () => {
@@ -30,21 +29,25 @@ const LoginScreen = () => {
     }
 
     return (
-        <form onSubmit={submitHandler}>
-            <label>Email</label>
-            <input type="email" placeholder="Enter email"
-                onChange= {(e) => setEmail(e.target.value)} value={email} />
-            
-            <label>Password</label>
-            <input type="password" placeholder="Enter password"
-                onChange= {(e) => setPassword(e.target.value)} value={password} />
+        <section className="login-screen w-full relative pt-5 flex justify-center ">
+            <img className='w-full absolute top-0' src='/images/formbackground.jpg' alt='form background'/>
+            <form className='flex flex-col justify-center px-5 my-auto' onSubmit={submitHandler}>
+                <h1 className='text-center text-2xl text-primary'>Login</h1>
+                <label className='text-xl pb-2'>Email</label>
+                <input type="email" className='outline-none px-2 py-1 rounded-md w-60' placeholder="Enter email"
+                    onChange= {(e) => setEmail(e.target.value)} value={email} />
+                
+                <label className='text-xl pt-3 pb-1'>Password</label>
+                <input type="password" className='outline-none px-2 py-1 rounded-md' placeholder="Enter password"
+                    onChange= {(e) => setPassword(e.target.value)} value={password} />
 
-            <button type="submit" >Sign In</button>
-            
-            <p>New Customer? <Link to= { redirect ? `/register?redirect=${redirect}` : '/register'}>
-                Register</Link>
-            </p>
-        </form>
+                <button className='btn border-solid border-2 text-primary w-15 py-2 px-4 my-4 font-bold rounded-md text-center mx-auto' type="submit" >Sign In</button>
+                
+                <p>New Customer? <Link to= { redirect ? `/register?redirect=${redirect}` : '/register'}>
+                    <span className='text-primary'>Register</span></Link>
+                </p>
+            </form>
+        </section>
     )
 }
 
